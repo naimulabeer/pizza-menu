e73190903b78e4ac0eb0b39c423af1f7982088e7
abeer117 <nabeer161117@bscse.uiu.ac.bd>
2023-06-27 23:05:33 +0600

    creating more components

--- src/index.js
+++ src/index.js
@@ -49,9 +49,31 @@ const pizzaData = [
 function App() {
   return (
     <div>
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+
+function Header() {
+  return <h1>Fast React Pizza Co.</h1>;
+}
+
+function Menu() {
+  return (
+    <div>
+      <h2>Our Menu</h2>
+      <Pizza />
       <Pizza />
       <Pizza />
     </div>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>{new Date().toLocaleTimeString()} We're Currently Open!</footer>
   );
 }
 
