1be29e269d001c05b041302c61f4209c7a0dabc0
abeer117 <nabeer161117@bscse.uiu.ac.bd>
2023-06-28 03:38:33 +0600

    setting classes and text conditionally

--- src/index.js
+++ src/index.js
@@ -93,11 +93,14 @@ function Pizza({ pizzaObj }) {
   // if (pizzaObj.soldOut) return null;
 
   return (
-    <li className="pizza">
-      <img src={pizzaObj.photoName} alt="Pizza Prosciutto" />
+    <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
+      <img src={pizzaObj.photoName} alt={pizzaObj.name} />
       <h3>{pizzaObj.name}</h3>
       <p>{pizzaObj.ingredients}</p>
-      <span>{pizzaObj.price}</span>
+
+      {/* {pizzaObj.soldOut ? <span>SOLD OUT</span> : <span>{pizzaObj.price}</span>} */}
+
+      <span>{pizzaObj.soldOut ? "Sold Out" : pizzaObj.price}</span>
     </li>
   );
 }
